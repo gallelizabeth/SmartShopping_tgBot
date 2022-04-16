dc7239416d93be7cc7d9e9837a441fc49db1e8ce
kotik <90791523+gallelizabeth@users.noreply.github.com>
2022-04-16 12:36:16 +0300

    обработка запроса(тест)

--- bot_input.py
+++ bot_input.py
@@ -1,6 +1,7 @@
 import logging
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
+# Запускаем логгирование
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
@@ -9,17 +10,28 @@ logger = logging.getLogger(__name__)
 
 TOKEN = '5182243678:AAH315moUblzcJYSXYfzS57jIZxNUeVqDMU'
 
+def start(update, context):
+    update.message.reply_text(
+        "Привет! Я посчитаю количество символов твоего сообщений. Напишите мне что-нибудь, и я пришлю тебе число!")
 
-def echo(update, context):
-    update.message.reply_text(update.message.text)
+def counter(update, context):
+    answer = 'В вашем сообщении '
+    if len(update.message.text) == 1:
+        update.message.reply_text(answer + str(len(update.message.text)) + ' символ.')
+    elif 3 <= len(update.message.text) <= 5:
+        update.message.reply_text(answer + str(len(update.message.text)) + ' символа.')
+    else:
+        update.message.reply_text(answer + str(len(update.message.text)) + ' символов.')
 
 
 def main():
     updater = Updater(TOKEN)
     dp = updater.dispatcher
-
-    text_handler = MessageHandler(Filters.text, echo)
-
+    # После регистрации обработчика в диспетчере
+    # эта функция будет вызываться при получении сообщения
+    # с типом "текст", т. е. текстовых сообщений.
+    text_handler = MessageHandler(Filters.text, counter)
+    dp.add_handler(CommandHandler("start", start))
     dp.add_handler(text_handler)
     updater.start_polling()
 
