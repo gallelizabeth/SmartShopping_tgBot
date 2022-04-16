64a4b39e8db86dbf14660ff583d4732f34dad38f
kotik <90791523+gallelizabeth@users.noreply.github.com>
2022-04-16 17:11:10 +0300

    отправка запросов и обработка

--- bot_input.py
+++ bot_input.py
@@ -10,37 +10,44 @@ logger = logging.getLogger(__name__)
 
 TOKEN = '5182243678:AAH315moUblzcJYSXYfzS57jIZxNUeVqDMU'
 
+done_address = False
+done_name = False
+
 
 def start(update, context):
     update.message.reply_text(
-        "Привет! Я помогу вам составить список покупок и найти нужный магазин :) "
-        "Давай приступим!")
-    login(update, context)
+        "Привет! Я помогу  тебе составить список покупок и найти нужный магазин, "
+        "но для начала, зарегистрируйся с помощью команды /login \n"
+        "если тебе понадобится помощь, вызови /help")
 
 
 def login(update, context):
-    update.message.reply_text("Для начала, давай зарегестрируемся."
-                              "Введи как к тебе обращаться и ваш адрес через точку с запятой (;)")
-    input_text = update.message.text
-    name, adress = input_text.split(';')
-    search(update, context)
+    do_username(update, context)
 
 
-def search(update, context):
-    update.message.reply_text("Теперь, выбери что хочешь сделать далее: "
-                              "составить список, редактировать профиль, маршрут до магазина")
-    check_answer(update, context)
+def do_username(update, context):
+    global done_name
+    update.message.reply_text("Введи как я могу к вам обращаться")
+    name = update.message.text
+    if name != '':
+        done_name = True
+        write_address(update, context)
 
 
+def write_address(update, context):
+    global done_address
+    update.message.reply_text("Введи ваш адрес")
+    address = update.message.text
+    if address != '':
+        done_address = True
 
-def check_answer(update, context):
-    input_text = update.message.text
-    if input_text == 'составить список':
-        make_a_list(update, context)
-    elif input_text == 'редактировать профиль':
-        make_a_way(update, context)
-    elif input_text == 'маршрут до магазина':
-        edit_profil(update, context)
+
+def help_me(update, context):
+    update.message.reply_text("Ты сможешь управлять мной используя эти команды: \n"
+                              "/editprifile – редактировать профиль \n"
+                              "/makealist – собрать список \n"
+                              "/foundshop – найти ближайший магазин \n"
+                              "нажми /help если тебе понадобиться помощь")
 
 
 def make_a_list(update, context):
@@ -48,23 +55,30 @@ def make_a_list(update, context):
 
 
 def make_a_way(update, context):
-    update.message.reply_text('Подключение к профилю..')
+    if done_address:
+        update.message.reply_text('Поиск..')
+    else:
+        update.message.reply_text('Упс! Похоже, вы не заполнили мини-профиль')
 
 
-def edit_profil(update, context):
-    update.message.reply_text('Поиск..')
+def edit_profile(update, context):
+    if done_name:
+        update.message.reply_text('Подключение к профилю..')
+    else:
+        update.message.reply_text('Упс! Похоже, вы не заполнили мини-профиль')
 
 
 def main():
     updater = Updater(TOKEN)
     dp = updater.dispatcher
-    # После регистрации обработчика в диспетчере
-    # эта функция будет вызываться при получении сообщения
-    # с типом "текст", т. е. текстовых сообщений.
-    text_handler = MessageHandler(Filters.text, search)
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("login", login))
+    dp.add_handler(CommandHandler("editprifile", edit_profile))
+    dp.add_handler(CommandHandler("makealist", make_a_list))
+    dp.add_handler(CommandHandler("foundshop", make_a_way))
+    dp.add_handler(CommandHandler("help", help_me))
+    # "/editlist – редактировать список \n"
 
-    dp.add_handler(text_handler)
     updater.start_polling()
 
     updater.idle()
