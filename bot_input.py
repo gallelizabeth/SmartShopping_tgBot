a678fb50ab5f9dd6bbd4c143bdc7f1249b89b6e5
kotik <90791523+gallelizabeth@users.noreply.github.com>
2022-04-16 23:36:08 +0300

    проверка на правильный ответ, доработаны сообщения от бота

--- bot_input.py
+++ bot_input.py
@@ -11,43 +11,32 @@ logger = logging.getLogger(__name__)
 TOKEN = '5182243678:AAH315moUblzcJYSXYfzS57jIZxNUeVqDMU'
 
 done_address = False
-done_name = False
 
 
 def start(update, context):
-    update.message.reply_text(
-        "Привет! Я помогу  тебе составить список покупок и найти нужный магазин, "
-        "но для начала, зарегистрируйся с помощью команды /login \n"
-        "если тебе понадобится помощь, вызови /help")
-
-
-def login(update, context):
-    do_username(update, context)
-
-
-def do_username(update, context):
-    global done_name
-    update.message.reply_text("Введи как я могу к вам обращаться")
-    name = update.message.text
-    if name != '':
-        done_name = True
-        write_address(update, context)
+    update.message.reply_text("Привет! Я помогу тебе составить список покупок и найти нужный магазин")
+    update.message.reply_text("Рекомендую задать свой адрес командой /setaddress для более качественного"
+                              " использования \n"
+                              "Так же, ты сможешь управлять мной используя эти команды: \n"
+                              "/editaddress – редактировать профиль \n"
+                              "/makealist – собрать список \n"
+                              "/foundshop – найти ближайший магазин \n"
+                              "нажми /help если тебе понадобиться помощь")
 
 
 def write_address(update, context):
     global done_address
-    update.message.reply_text("Введи ваш адрес")
+    update.message.reply_text("Укажите ваш адрес")
     address = update.message.text
-    if address != '':
+    if address[0] != '/':
         done_address = True
+    return address
 
 
 def help_me(update, context):
-    update.message.reply_text("Ты сможешь управлять мной используя эти команды: \n"
-                              "/editprifile – редактировать профиль \n"
-                              "/makealist – собрать список \n"
-                              "/foundshop – найти ближайший магазин \n"
-                              "нажми /help если тебе понадобиться помощь")
+    update.message.reply_text("Если у тебя что то не получается, попробуй поменять регистр сообщения. "
+                              "Если же просблема не исчезла, советуем обратиться в поддержку \n"
+                              "Приятной эксплуатации :)")
 
 
 def make_a_list(update, context):
@@ -58,22 +47,22 @@ def make_a_way(update, context):
     if done_address:
         update.message.reply_text('Поиск..')
     else:
-        update.message.reply_text('Упс! Похоже, вы не заполнили мини-профиль')
+        update.message.reply_text('Упс! Похоже, вы не указали адрес')
 
 
-def edit_profile(update, context):
-    if done_name:
+def edit_address(update, context):
+    if done_address:
         update.message.reply_text('Подключение к профилю..')
     else:
-        update.message.reply_text('Упс! Похоже, вы не заполнили мини-профиль')
+        update.message.reply_text('Упс! Похоже, вы не указали адрес')
 
 
 def main():
     updater = Updater(TOKEN)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("login", login))
-    dp.add_handler(CommandHandler("editprifile", edit_profile))
+    dp.add_handler(CommandHandler("setaddress", write_address))
+    dp.add_handler(CommandHandler("editaddress", edit_address))
     dp.add_handler(CommandHandler("makealist", make_a_list))
     dp.add_handler(CommandHandler("foundshop", make_a_way))
     dp.add_handler(CommandHandler("help", help_me))
