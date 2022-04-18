0a9f67d32dc8e3722a4d0490114954538a26a179
kotik <90791523+gallelizabeth@users.noreply.github.com>
2022-04-18 23:50:09 +0300

    initial commit

--- bot_input.py
+++ bot_input.py
@@ -15,12 +15,13 @@ done_address = False
 
 def start(update, context):
     update.message.reply_text("Привет! Я помогу тебе составить список покупок и найти нужный магазин")
-    update.message.reply_text("Рекомендую задать свой адрес командой /setaddress для более качественного"
+    update.message.reply_text("Рекомендую задать свой адрес командой /setaddress, для более качественного"
                               " использования \n"
                               "Так же, ты сможешь управлять мной используя эти команды: \n"
-                              "/editaddress – редактировать профиль \n"
+                              "/editaddress – изменить адрес \n"
                               "/makealist – собрать список \n"
-                              "/foundshop – найти ближайший магазин \n"
+                              "/found shop – найти ближайший магазин \n"
+                              "/editlist – редактировать список \n"
                               "нажми /help если тебе понадобиться помощь")
 
 
@@ -35,8 +36,8 @@ def write_address(update, context):
 
 
 def help_me(update, context):
-    update.message.reply_text("Если у тебя что то не получается, попробуй поменять регистр сообщения. "
-                              "Если же просблема не исчезла, советуем обратиться в поддержку \n"
+    update.message.reply_text("Если у тебя что-то не получается, попробуй поменять регистр сообщения. "
+                              "Если же проблема не исчезла, советуем обратиться в поддержку \n"
                               "Приятной эксплуатации :)")
 
 
@@ -44,7 +45,11 @@ def make_a_list(update, context):
     update.message.reply_text('Составляем список..')
 
 
-def make_a_way(update, context):
+def edit_list(update, context):
+    pass
+
+
+def find_shop(update, context):
     if done_address:
         update.message.reply_text('Поиск..')
     else:
@@ -66,9 +71,9 @@ def main():
     dp.add_handler(CommandHandler("setaddress", write_address))
     dp.add_handler(CommandHandler("editaddress", edit_address))
     dp.add_handler(CommandHandler("makealist", make_a_list))
-    dp.add_handler(CommandHandler("foundshop", make_a_way))
+    dp.add_handler(CommandHandler("editlist", edit_list))
+    dp.add_handler(CommandHandler("foundshop", find_shop))
     dp.add_handler(CommandHandler("help", help_me))
-    # "/editlist – редактировать список \n"
 
     updater.start_polling()
 
