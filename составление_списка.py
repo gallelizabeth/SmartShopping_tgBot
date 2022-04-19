0c4b0150a6cafe639b94aa61e913ab0c2ecfba86
Samira Amirova <0244503@gmail.com>
2022-04-19 12:07:57 +0300

    cохранение списка + название

--- составление_списка.py
+++ составление_списка.py
@@ -1,6 +1,6 @@
 import logging
-
 import telegram
+from telegram import ReplyKeyboardMarkup
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
 logging.basicConfig(
@@ -9,10 +9,16 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-TOKEN = 'TOKEN'
+TOKEN = '5349979559:AAHcXnyvYcT_ETNfqlJiJNC3uDjicwmU3mM'
 list_prod = []
 shopping_list = ''
 all_lists = dict()
+creating = False
+saving = False
+
+# keyboard
+save_items = [['Да', 'Нет']]
+markup_1 = ReplyKeyboardMarkup(save_items, one_time_keyboard=True)
 
 
 def start(update, context):
@@ -27,29 +33,49 @@ def start(update, context):
 
 
 def create_list(update, context):
+    global creating
     update.message.reply_text('Введи название продукта и его количество через пробел')
     update.message.reply_text('Когда закончишь, напиши СТОП')
+    creating = True
 
 
-def add_item(update, context):
-    global shopping_list
-    list_prod.append(update.message.text)
-    if update.message.text != 'СТОП':
-        print(update.message.text)
-    else:
-        list_prod.pop(list_prod.index(list_prod[-1]))
-        update.message.reply_text('Вот твой список:')
-        for i in sorted(list_prod):
-            if not i[-1].isalpha():
-                shopping_list = shopping_list + i + ' шт' + '\n'
-            else:
-                shopping_list = shopping_list + i + '\n'
-        save_list(shopping_list)
-        update.message.reply_text(shopping_list)
+def reaction(update, context):
+    global shopping_list, creating, saving, all_lists
+    if creating:
+        list_prod.append(update.message.text)
+        if update.message.text != 'СТОП' and update.message.text != 'стоп':
+            print(update.message.text)
+        else:
+            list_prod.pop(list_prod.index(list_prod[-1]))
+            update.message.reply_text('Вот твой список:')
+            for i in sorted(list_prod):
+                if not i[-1].isalpha():
+                    shopping_list = shopping_list + i + ' шт' + '\n'
+                else:
+                    shopping_list = shopping_list + i + '\n'
+            update.message.reply_text(shopping_list)
+            creating = False
+            save_list(update, context, shopping_list)
+    elif update.message.text == 'Да' and not creating:
+        update.message.reply_text('Дай название списку')
+        saving = True
+    elif update.message.text == 'Нет' and not creating:
+        update.message.reply_text('Список удален')
+    elif saving:
+        name = update.message.text
+        all_lists[name] = shopping_list
+        shopping_list = ''
+        update.message.reply_text(f'Список сохранен как "{name}"')
+        saving = False
 
 
-def save_list(saving_list):
-    # сохранение в базу данных или куда-то там еще
+def save_list(update, context, saving_list):  # сохранение списка в бд
+    global saving
+    update.message.reply_text("Ты хочешь сохранить список?", reply_markup=markup_1)
+
+
+def see_list(update, context):  # посмотреть список
+    pass
 
 
 def main():
@@ -57,7 +83,7 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("makealist", create_list))
-    dp.add_handler(MessageHandler(Filters.text, add_item))
+    dp.add_handler(MessageHandler(Filters.text, reaction))
 
     updater.start_polling()
 
