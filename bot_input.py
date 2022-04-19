bbcbf696cd4166da6cd0b92e61cc3bb40497a5aa
kotik <90791523+gallelizabeth@users.noreply.github.com>
2022-04-20 02:09:44 +0300

    редактикование списка(нуждается в исправлении)

--- bot_input.py
+++ bot_input.py
@@ -2,6 +2,8 @@ import logging
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
 # Запускаем логгирование
+from составление_списка import save_list
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
@@ -11,6 +13,8 @@ logger = logging.getLogger(__name__)
 TOKEN = '5182243678:AAH315moUblzcJYSXYfzS57jIZxNUeVqDMU'
 
 done_address = False
+add_prod = []
+shopping_list = ''
 
 
 def start(update, context):
@@ -45,16 +49,42 @@ def make_a_list(update, context):
     import sqlite3
     con = sqlite3.connect("lists_db.db")
     cur = con.cursor()
-    quary = """INSERT INTO saves(name_of_list, list) VALUES (""" + str(name) + """,""" + str(
-        list_prod) + """)"""
-    cur.execute(quary)
+    # quary = """INSERT INTO saves(name_of_list, list) VALUES (""" + str(name) + """,""" + str(
+    # list_prod) + """)"""
+    # cur.execute(quary)
     con.commit()
     con.close()
     update.message.reply_text('Составляем список..')
 
 
 def edit_list(update, context):
-    pass
+    global add_prod, shopping_list
+    update.message.reply_text('Укажи название списка в который ты хочешь добавить продукт(ы)')
+    list_name = update.message.text
+    update.message.reply_text('Укажи продукты которые ты хочешь добавить')
+    update.message.reply_text('Когда закончишь, напиши СТОП')
+    add_prod.append(update.message.text)
+    if update.message.text != 'СТОП' and update.message.text != 'стоп':
+        print(update.message.text)
+    else:
+        add_prod.pop(add_prod.index(add_prod[-1]))
+        update.message.reply_text('Вот твой список:')
+        for i in sorted(add_prod):
+            if not i[-1].isalpha():
+                shopping_list = shopping_list + i + ' шт' + '\n'
+            else:
+                shopping_list = shopping_list + i + '\n'
+        update.message.reply_text(shopping_list)
+        save_list(update, context, shopping_list)
+    import sqlite3
+    con = sqlite3.connect("lists_db.db")
+    cur = con.cursor()
+    quary = """INSERT INTO saves(list) VALUES (""" + str(add_prod) + """)"""
+    saved_list = """SELECT list FROM saves WHERE name_of_list = (""" + str(list_name) + """)"""
+    cur.execute(quary)
+    con.commit()
+    con.close()
+    update.message.reply_text(saved_list)
 
 
 def find_shop(update, context):
@@ -65,11 +95,10 @@ def find_shop(update, context):
 
 
 def edit_address(update, context):
-    if done_address:
-        update.message.reply_text('Похоже, ты хочешь поменять адрес')
-        write_address(update, context)
-    else:
-        update.message.reply_text('Упс! Похоже, ты не указал адрес')
+    global writing_adrs
+    writing_adrs = False
+    update.message.reply_text("Укажи адрес на который ты хочешь сминить указанный")
+    writing_adrs = True
 
 
 def main():
