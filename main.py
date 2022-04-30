99a4138e53a9df4cd0c1afc86257b7346bb07cc3
Samira Amirova <0244503@gmail.com>
2022-04-30 03:02:45 +0300

    осталось исправить ошибку при добавлении

--- main.py
+++ main.py
@@ -6,27 +6,24 @@ import telegram
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
+from data import db_session
+from data.users import User
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
 
 logger = logging.getLogger(__name__)
 
-TOKEN = '5182243678:AAH315moUblzcJYSXYfzS57jIZxNUeVqDMU'
+TOKEN = '5349979559:AAHcXnyvYcT_ETNfqlJiJNC3uDjicwmU3mM'
 
 markup = ReplyKeyboardRemove()
 
-add_prod = []
-shopping_list = ''
-address = ''
-coordinates_shop = ''
-all_lists = dict()
 done_address = False
 creating = False
 saving = False
 writing_adrs = False
 save = False
-list_prod = []
 check = False
 add_to_list = False
 del_from_list = False
@@ -45,6 +42,7 @@ markup_list = ReplyKeyboardMarkup(check_list, one_time_keyboard=True)
 
 
 def start(update, context):
+    reg(update, context)
     update.message.reply_text("Привет! Я помогу тебе составить список покупок и найти нужный магазин",
                               reply_markup=markup)
     update.message.reply_text("Рекомендую задать свой адрес командой /setaddress для более качественного"
@@ -57,6 +55,19 @@ def start(update, context):
                               "заданного адреса и узнать расстояние между ними\n"
                               "нажмите /help , если тебе понадобится помощь\n"
                               "БОТ ОСУЩЕСТВЛЯЕТ РАБОТУ ТОЛЬКО В ПРЕДЕЛАХ МОСКВЫ")
+
+
+def reg(update, context):
+    user = User()
+    user.name = update.message.chat.id
+    user.address = ''
+    user.shopping_list = ''
+    user.coordinates_shop = ''
+    user.list_prod = ''
+
+    if db_sess.query(User).filter(User.name == update.message.chat.id).count() == 0:
+        db_sess.add(user)
+    db_sess.commit()
 
 
 def write_address(update, context):
@@ -73,76 +84,76 @@ def help_me(update, context):
 
 def create_list(update, context):
     global creating
+    user = db_sess.query(User).filter(User.name == update.message.chat.id).first()
+
     update.message.reply_text('Введи название продукта и его количество через пробел')
     update.message.reply_text('Когда закончишь, напиши СТОП')
+    user.list_prod = ''
+    db_sess.commit()
     creating = True
 
 
 def reaction(update, context):
-    global shopping_list, creating, saving, all_lists, list_prod, route, address, coordinates_shop, route_done
-    global writing_adrs, done_address, add_prod, save, check, add_to_list, del_from_list, del_list, to_map
+    global creating, saving, route
+    global route_done
+    global writing_adrs, done_address, save, check, add_to_list, del_from_list, del_list, to_map
+    user = db_sess.query(User).filter(User.name == update.message.chat.id).first()
     if creating:
         print('creating')
-        list_prod.append(update.message.text)
         if update.message.text == 'СТОП' or update.message.text == 'стоп' or update.message.text == 'Стоп':
-            list_prod.pop(list_prod.index(list_prod[-1]))
+            user.list_prod = user.list_prod[:-1]
             get_list(update, context)
             creating = False
-            save_list(update, context, shopping_list)
+            save_list(update, context, user.shopping_list)
+        else:
+            user.list_prod = user.list_prod + update.message.text + ','
     elif save:
         print('save')
         if update.message.text == 'Да':
+            db_sess.commit()
             update.message.reply_text('Список успешно сохранён', reply_markup=markup)
             save = False
         elif update.message.text == 'Нет':
-            shopping_list = ''
-            list_prod = []
+            user.shopping_list = ''
+            user.list_prod = ''
+            db_sess.commit()
             save = False
             update.message.reply_text('Список удален', reply_markup=markup)
 
     elif writing_adrs:
         print('writing_adrs')
-        address = update.message.text
+        user.address = update.message.text
+        db_sess.commit()
         update.message.reply_text("Ты успешно установил адрес")
         done_address = True
         writing_adrs = False
 
     elif check:
         print('check')
-        if update.message.text == 'Добавить элемент':
-            add_to_list = True
-            check = False
-            update.message.reply_text('Укажи продукты, которые ты хочешь добавить\nКогда закончишь, напиши СТОП')
-        elif update.message.text == 'Удалить элемент':
-            del_from_list = True
-            check = False
-            update.message.reply_text('Укажи продукты, которые ты хочешь удалить\nКогда закончишь, напиши СТОП')
-        elif update.message.text == 'Удалить список':
-            list_prod = []
-            update.message.reply_text('Список успешно удалён')
-            check = False
+        function_edit_list(update, context)
 
     elif add_to_list:
         print('add_to_list')
         if update.message.text != 'СТОП' and update.message.text != 'стоп' and update.message.text != 'Стоп':
-            list_prod.append(update.message.text)
+            user.list_prod = ',' + user.list_prod + update.message.text + ','
         else:
-            shopping_list = ''
-            for i in sorted(list_prod):
-                if i == 'Добавить элемент':
-                    list_prod.remove(i)
-                else:
-                    get_list(update, context)
+            user.list_prod = user.list_prod[:-1]
+            user.shopping_list = ''
+            db_sess.commit()
+            get_list(update, context)
             add_to_list = False
             check = False
 
     elif del_from_list:
         print('del_from_list')
+        list_user_prod = str(user.list_prod).split(',')
         if update.message.text != 'СТОП' and update.message.text != 'стоп'\
                 and update.message.text != 'Стоп':
-            for item in list_prod:
+            for item in list_user_prod:
                 if update.message.text in item:
-                    list_prod.remove(item)
+                    list_user_prod.remove(item)
+                    user.list_prod = ','.join(list_user_prod)
+            db_sess.commit()
         else:
             get_list(update, context)
             del_from_list = False
@@ -150,20 +161,21 @@ def reaction(update, context):
 
     elif route:
         print('route')
-        coordinates_shop = update.message.text
+        user.coordinates_shop = update.message.text
         route = False
         print('to_map')
-        make_map(update, context, coordinates_shop)
+        make_map(update, context, user.coordinates_shop)
     else:
         print('else')
-        if update.message.text != address:
+        if update.message.text != user.address:
             update.message.reply_text('Прости, я тебя не понял. Воспользуйся одной из команд /start',
                                       reply_markup=markup)
 
 
 def make_map(update, context, shop):
+    user = db_sess.query(User).filter(User.name == update.message.chat.id).first()
     global to_map
-    long_h, lat_h = coordinates(update, context, address)
+    long_h, lat_h = coordinates(update, context, user.address)
     shop = '+'.join((shop.split()))
     geocoder_request = 'https://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&' \
                        f'geocode=Москва+{shop}&format=json'
@@ -222,6 +234,26 @@ def edit_list(update, context):
     choose(update, context)
 
 
+def function_edit_list(update, context):
+    user = db_sess.query(User).filter(User.name == update.message.chat.id).first()
+    global add_to_list, check, del_from_list, list_prod
+    if update.message.text == 'Добавить элемент':
+        add_to_list = True
+        check = False
+        update.message.reply_text('Укажи продукты, которые ты хочешь добавить\n'
+                                  'Когда закончишь, напиши СТОП', reply_markup=markup)
+    elif update.message.text == 'Удалить элемент':
+        del_from_list = True
+        check = False
+        update.message.reply_text('Укажи продукты, которые ты хочешь удалить\n'
+                                  'Когда закончишь, напиши СТОП', reply_markup=markup)
+    elif update.message.text == 'Удалить список':
+        user.list_prod = ''
+        db_sess.commit()
+        update.message.reply_text('Список успешно удалён', reply_markup=markup)
+        check = False
+
+
 def choose(update, context):
     global check
     update.message.reply_text('Выбери один из вариантов', reply_markup=markup_list)
@@ -257,15 +289,20 @@ def find_shop(update, context):
 
 
 def get_list(update, context):
-    global shopping_list, list_prod
-    update.message.reply_text('Вот твой список:')
-    shopping_list = ''
-    for i in list_prod:
-        if not i[-1].isalpha():
-            shopping_list = shopping_list + i + ' шт' + '\n'
-        else:
-            shopping_list = shopping_list + i + '\n'
-    update.message.reply_text(shopping_list)
+    user = db_sess.query(User).filter(User.name == update.message.chat.id).first()
+    if len(user.list_prod) != 0:
+        print(user.list_prod)
+        update.message.reply_text('Вот твой список:')
+        user.shopping_list = ''
+        res_list = str(user.list_prod).split(',')
+        for i in res_list:
+            if not i[-1].isalpha() or not i[-2].isalpha():
+                user.shopping_list = user.shopping_list + i + ' шт' + '\n'
+            else:
+                user.shopping_list = user.shopping_list + i + '\n'
+        update.message.reply_text(user.shopping_list)
+    else:
+        update.message.reply_text('Список пуст')
 
 
 def main():
@@ -286,4 +323,6 @@ def main():
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/smarts.db")
+    db_sess = db_session.create_session()
     main()
